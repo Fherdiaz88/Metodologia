e994dd1ebd7733327853d5e8795829ee4cffd88a
Fernando Diaz <jfdiaz.2@sudamericano.edu.ec>
2024-06-19 15:18:17 -0500

    semana11

---
+++ Tareas-semana11-arrays/Pregunta 2/index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+let array=["0","2","4","8"];
+
+
+
+function sum(array){
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            let resultado= num[i] +""+ num[j];
+            alert(resultado);
+        }
+    }
+}
+
+
+
+
+
